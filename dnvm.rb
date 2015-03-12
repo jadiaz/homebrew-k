f29aed69115ea1183226167d639891c6363b61f9
Jose Diaz <jose.a.diaz@aguaseca.com>
2015-03-12 00:36:09 -0700

    fix file not found

--- dnvm.rb
+++ dnvm.rb
@@ -3,17 +3,14 @@ require "formula"
 class Dnvm < Formula
   homepage "https://www.github.com/aspnet/Home"
   version "1.0.0-beta4"
-  url "https://github.com/aspnet/Home.git"
-
-  head "https://github.com/aspnet/Home.git", :branch => 'dev'
+  url "https://github.com/aspnet/Home.git", :branch => 'dev'
 
   depends_on "mono" => :recommended
 
   def install
-    libexec.install "dnvminstall.sh"
-    (libexec + "dnvm.sh").chmod 0755
-    (libexec + "mono").make_symlink Formula["mono"].opt_bin/"mono"
-    system "bash -c 'source #{libexec}/dnvm.sh; dnvm upgrade'"
+    chmod 0755, "dnvminstall.sh"
+    system './dnvminstall.sh'
+    system 'source dnvm.sh; dnvm upgrade'
   end
 
   def caveats; <<-EOS.undent
